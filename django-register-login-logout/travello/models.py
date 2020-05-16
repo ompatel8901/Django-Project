455af8ee93f893ad610b29ba720884d196902919
ompatel8901 <ompatel6294@gmail.com>
2020-05-16 21:34:56 +0530

    First Django Project

---
+++ django-register-login-logout/travello/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class destination(models.Model):
+    name=models.CharField(max_length=180)
+    img=models.ImageField(upload_to='pics')
+    desc=models.TextField()
+    price=models.IntegerField()
+    offer=models.BooleanField(default=False)
